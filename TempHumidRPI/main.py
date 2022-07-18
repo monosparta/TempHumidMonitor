58999b02597b60cf14df3d5a1c20ce166bd6cad8
VanLinLin <aas12as12as12tw@gmail.com>
2022-07-18 11:17:42 +0800

    feat(RPI): add led function

--- TempHumidRPI/main.py
+++ TempHumidRPI/main.py
@@ -1,23 +1,23 @@
 from pyclbr import Class
 import time
-import Adafruit_DHT
+import dht11
 import LCD_drive as driver
 import mqtt_publish
-import os
 import json
+import RPi.GPIO as gpio
 
 
+# read the json file
 def read_json() -> dict:
     try:
-
-        with open("config.json") as f:
+        with open("temp_humid_project/TempHumidRPI/config.json") as f:
             return json.loads(f.read())
-
     except FileNotFoundError:
         raise FileNotFoundError("Couldn't find a setting.json file containing mqtt and gpio settings!")
 
 
-def connect_to_mqtt(data) -> Class:
+# connect to mqtt
+def connect_to_mqtt(data: dict) -> Class:
     return mqtt_publish.MQTT(
         data["mqtt"]["username"],
         data["mqtt"]["password"],
@@ -28,12 +28,18 @@ def connect_to_mqtt(data) -> Class:
 
 def main() -> None:
 
+    gpio.setmode(gpio.BOARD)
+
     data = read_json()
 
     mqtt_publisher = connect_to_mqtt(data)
 
+    gpio.setup(data["gpio"]["green_led"], gpio.OUT)
+
     # 建立LCD物件
     mylcd = driver.lcd()
+
+    mylcd.lcd_clear()
 
     print('按下 Ctrl-C 可停止程式')
     mylcd.lcd_display_string("Loading.")
@@ -42,27 +48,27 @@ def main() -> None:
     time.sleep(0.3)
     mylcd.lcd_display_string("Loading...")
 
-    # 等待0.5秒
     time.sleep(0.5)
-
     try:
         while True:
             # 偵測溫溼度資料
-            h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, data['gpio']["DHT11"])
+            envir_data = dht11.DHT11(pin=7).read()
 
             # 判斷資料是否為None型別
-            if h is not None and t is not None:
+            if envir_data.temperature or envir_data.humidity != 0:
+                gpio.output(data["gpio"]["green_led"], True)
+
                 # 顯示溫溼度資料
-                print(F'溫度={t:0.1f}度C 濕度={h:0.1f}%')
+                print(F'溫度={envir_data.temperature:0.1f}{chr(223)}C 濕度={envir_data.humidity:0.1f}%')
 
                 # LCD第一行顯示溫度
-                mylcd.lcd_display_string(F" Temp: {t:0.1f} {chr(223)}C", 1, clean=True)
+                mylcd.lcd_display_string(F" Temp: {envir_data.temperature:0.1f} {chr(223)}C", 1, clean=True)
 
                 # LCD第二行顯示濕度
-                mylcd.lcd_display_string(F"Humid: {h:0.1f}  %", 2)
+                mylcd.lcd_display_string(F"Humid: {envir_data.humidity:0.1f}  %", 2)
 
                 # 發送mqtt資料
-                mqtt_publisher.send_message(temperature=t, humidity=h)
+                mqtt_publisher.send_message(temperature=envir_data.temperature, humidity=envir_data.humidity)
             else:
                 print('讀取失敗，重新讀取。')
 
@@ -71,6 +77,11 @@ def main() -> None:
     except KeyboardInterrupt:
         print('關閉程式')
         mylcd.lcd_clear()
+        mylcd.lcd_display_string("See you!")
+        time.sleep(1)
+        mylcd.lcd_clear()
+        gpio.output(data["gpio"]["green_led"], False)
+        gpio.cleanup()
 
 
 if __name__ == "__main__":
