1b713cc733dcb6eaa9639afb46bc92abedbce98c
jimchen5209 <jimchen5209@gmail.com>
2022-07-19 09:53:35 +0800

    feat(Backend): Send data on connect

--- TempHumidBack/src/Components/Socket.ts
+++ TempHumidBack/src/Components/Socket.ts
@@ -21,16 +21,30 @@ interface SocketData {
 
 export class Socket {
     private readonly io: Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
-
+    private lastData: { temp: number, humid: number, time: string } = {
+        temp: 0,
+        humid: 0,
+        time: ""
+    };
     /**
      * Socket.io Server
      * @param core App Core Instance
      */
     constructor(core: Core) {
         this.io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(core.server.server);
+
+        // Store last data
+        core.mqtt.subscribe(core.config.mqtt.topic, (_, payload) => {
+            const data = JSON.parse(payload.toString());
+            this.lastData = { temp: data.Temperature, humid: data.Humidity, time: data.Time }
+        })
+
         // On client connected to server via websocket
         this.io.on('connection', socket => {
             socket.on('disconnect', () => { });
+
+            // Send last data to client
+            socket.emit("update", this.lastData);
 
             // Send mqtt message to websocket
             core.mqtt.subscribe(core.config.mqtt.topic, (_, payload) => {
