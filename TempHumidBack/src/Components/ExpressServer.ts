bff0e3b7e00f2175e920fd2b5a9b0c8ec3c8c727
jimchen5209 <jimchen5209@gmail.com>
2022-07-22 16:05:15 +0800

    feat(Backend): Add Cors

--- TempHumidBack/src/Components/ExpressServer.ts
+++ TempHumidBack/src/Components/ExpressServer.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { Core } from '..';
 import { createServer } from 'http'
 
@@ -12,11 +13,20 @@ export class ExpressServer
      * @param core App Core Instance
      */
     constructor(core: Core) {
-        this.app.use(function (req, res, next) {
-            res.header("Access-Control-Allow-Origin", ['http://localhost/', 'http://192.168.168.113', 'http://mono202207-temp-humid.herokuapp.com/']); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
+        const allowedOrigins = ['http://localhost/', 'http://192.168.168.113', 'http://mono202207-temp-humid.herokuapp.com/']
+        this.app.use(cors({
+            origin: function (origin, callback) {
+                // allow requests with no origin 
+                // (like mobile apps or curl requests)
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    const msg = 'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+                    return callback(new Error(msg), false);
+                }
+                return callback(null, true);
+            }
+        }))
         
         // Get data from last 24 hours and average by hour
         this.app.get('/api/getLast24Hours', (req, res) => {
